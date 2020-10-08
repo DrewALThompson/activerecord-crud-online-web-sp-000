3daf8f0db60b8696d749947c09f63db5634952a6
Drew Thompson <drewallynthompson@gmail.com>
2020-10-08 09:21:38 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,9 +74,9 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  user = Movie.create(title: "Wat?")
-    
-  __
+  Movie.create(title: "Wat?")
+  movie = Movie.where(title: "Wat?")
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
